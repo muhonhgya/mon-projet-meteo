30f5d1566b8c60343fbcb386086c77c492a88bc7
Jiress Muhongya <giressemumbere42@gmail.com>
2025-09-04 20:55:20 +0100

    Premier commit projet météo

---
+++ Affiche météo.js
@@ -0,0 +1,35 @@
+
+async function getWeather() {
+    const city = document.getElementById("city").value;
+    
+    const url = `https://mini-meteo.onrender.com/weather?city=${city}`;
+
+
+    const weatherDiv = document.getElementById("weather");
+    weatherDiv.innerHTML = "⏳ Chargement...";
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data && data.name) { // On vérifie que la ville existe
+            weatherDiv.innerHTML = `
+              🌍 Ville: ${data.name} <br>
+              🌡 Température: ${data.main.temp}°C <br>
+              🌬 Vent: ${data.wind.speed} km/h<br>
+              💧 Humidité: ${data.main.humidity}%<br>
+              ☁ Météo: ${data.weather[0].description}
+            `;
+        } else {
+            weatherDiv.innerHTML = "❌ Ville introuvable";
+        }
+
+    } catch (err) {
+        weatherDiv.innerHTML = "⚠️ Erreur serveur ou ville introuvable";
+    }
+}
+
+// Permet d'appuyer sur Entrée
+document.getElementById("city").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") getWeather();
+});
---
+++ app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__, static_folder=".")
+CORS(app)  # <-- active CORS
+
+API_KEY = "f5b54e2d46494c7790ecbaa26080a871"
+
+@app.route("/")
+def index():
+    return send_from_directory(".", "Affiche météo.html")
+
+@app.route("/weather")
+def weather():
+    city = request.args.get("city")
+    if not city:
+        return jsonify({"error": "Ville manquante"}), 400
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=fr"
+    r = requests.get(url)
+    return jsonify(r.json())
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
